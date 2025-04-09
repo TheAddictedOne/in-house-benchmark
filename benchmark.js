81f70a9de39613e90707ec95e91c87c51845df3d
David Auk <auk.david@gmail.com>
2025-04-09 16:30:54 +0200

    Add more metrics

--- benchmark.js
+++ benchmark.js
@@ -8,18 +8,34 @@
   const marks = {} // Keep in memory marks
 
   /**
+   * Load DM script
+   */
+  const load_dm_script = async () => {
+    log_mark(performance.mark('dm_script_start'))
+    return new Promise((resolve) => {
+      const onLoad = () => {
+        log_mark(performance.mark('dm_script_end'))
+        resolve()
+      }
+      const script = document.createElement('script')
+      script.src = 'https://geo.dailymotion.com/libs/player/x136j6.js'
+      script.onload = onLoad
+      document.head.appendChild(script)
+    })
+  }
+
+  /**
    * Create DM player
-   * Set dm_* performance marks
    */
   const start_dm_player = async () => {
-    log_mark(performance.mark('dm_start'))
+    log_mark(performance.mark('dm_player_start'))
     return new Promise(async (resolve) => {
-      const dmPlayer = await dailymotion.createPlayer('dm-player', { video: 'x9hhim0' })
-      dmPlayer.on(
+      const player = await dailymotion.createPlayer('dm-player', { video: 'x9hhim0' })
+      player.on(
         dailymotion.events.VIDEO_PLAYING,
         () => {
-          log_mark(performance.mark('dm_end'))
-          setTimeout(dmPlayer.pause, PLAY_DURATION)
+          log_mark(performance.mark('dm_player_end'))
+          setTimeout(player.pause, PLAY_DURATION)
           resolve()
         },
         { once: true }
@@ -28,11 +44,27 @@
   }
 
   /**
+   * Load JW script
+   */
+  const load_jw_script = async () => {
+    log_mark(performance.mark('jw_script_start'))
+    return new Promise((resolve) => {
+      const onLoad = () => {
+        log_mark(performance.mark('jw_script_end'))
+        resolve()
+      }
+      const script = document.createElement('script')
+      script.src = 'https://content.jwplatform.com/libraries/IDzF9Zmk.js'
+      script.onload = onLoad
+      document.head.appendChild(script)
+    })
+  }
+
+  /**
    * Create JW player
-   * Set jw_* performance marks
    */
   const start_jw_player = async () => {
-    log_mark(performance.mark('jw_start'))
+    log_mark(performance.mark('jw_player_start'))
     return new Promise(async (resolve) => {
       const jwPlayer = await jwplayer('jw-player').setup({
         autostart: true,
@@ -40,7 +72,7 @@
         // playlist: "https://cdn.jwplayer.com/v2/media/pjntNKMa", // Video from NewsCorp benchmark
       })
       jwPlayer.once('firstFrame', () => {
-        log_mark(performance.mark('jw_end'))
+        log_mark(performance.mark('jw_player_end'))
         setTimeout(jwPlayer.pause, PLAY_DURATION)
         resolve()
       })
@@ -51,7 +83,7 @@
    * Display mark in page
    * @param {PerformanceMark} mark
    */
-  function log_mark(mark) {
+  const log_mark = (mark) => {
     marks[mark.name] = mark // Save mark in upper scope for later usage
     document.querySelector('#logs').innerHTML += `<div class="log-row">
         <span><code>${mark.name}</code></span>
@@ -65,10 +97,20 @@
    * @param {PerformanceMark} start
    * @param {PerformanceMark} end
    */
-  function log_diff(id, start, end) {
+  const log_diff = (id, start, end) => {
     const diff = Math.round(end.startTime - start.startTime)
-    document.querySelector(id).innerHTML +=
-      `<code>${start.name}</code> to <code>${end.name}</code> → ${diff}ms`
+    document.querySelector(id).innerHTML += `<div class="log-row">
+      <span><code>${start.name}</code> to <code>${end.name}</code></span>
+      <span class="font-mono">${diff}ms</span>
+    </div>`
+  }
+
+  const log_total = (id, start, end) => {
+    const diff = Math.round(end.startTime - start.startTime)
+    document.querySelector(id).innerHTML += `<div class="log-row">
+      <span>Total</span>
+      <span class="font-mono">${diff}ms</span>
+    </div>`
   }
 
   // ┌───────────────────────────────────────────────────────────────────────────────────────────────┐
@@ -76,9 +118,15 @@
   // │ Runtime                                                                                       │
   // │                                                                                               │
   // └───────────────────────────────────────────────────────────────────────────────────────────────┘
-  log_mark(window.performance.mark('origin', { startTime: 0 }))
+  log_mark(performance.mark('origin', { startTime: 0 }))
+  await load_dm_script()
   await start_dm_player()
+  await load_jw_script()
   await start_jw_player()
-  log_diff('#dm-diff', marks.dm_start, marks.dm_end)
-  log_diff('#jw-diff', marks.jw_start, marks.jw_end)
+  log_diff('#dm-diff', marks.dm_script_start, marks.dm_script_end)
+  log_diff('#dm-diff', marks.dm_player_start, marks.dm_player_end)
+  log_total('#dm-diff', marks.dm_script_start, marks.dm_player_end)
+  log_diff('#jw-diff', marks.jw_script_start, marks.jw_script_end)
+  log_diff('#jw-diff', marks.jw_player_start, marks.jw_player_end)
+  log_total('#jw-diff', marks.jw_script_start, marks.jw_player_end)
 })()
