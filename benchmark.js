935c7b131ceb3aeffb7c66f2c3530a72ed49910e
David Auk <auk.david@gmail.com>
2025-04-10 15:39:59 +0200

    Add preset for adless and preroll benchmarks

--- benchmark.js
+++ benchmark.js
@@ -7,35 +7,56 @@
   const PLAY_DURATION = 5100 // in ms. Arbitrary number to make the page feeling alive by playing videos for some seconds
   const marks = {} // Keep in memory marks
 
-  /**
-   * Load DM script
-   */
+  const autoreload_on_preset = (preset) => {
+    const node = document.querySelector('#preset')
+    node.value = preset
+    node.addEventListener('change', () => {
+      const url = new URL(location.href)
+      url.searchParams.set('preset', node.value)
+      document.location.href = url
+    })
+  }
+
+  const save_mark = (name) => {
+    const mark = performance.mark(name)
+    marks[name] = mark // Save mark in upper scope for later usage
+    document.querySelector('#logs').innerHTML += `<div class="log-row">
+        <span><code>${name}</code></span>
+        <span class="font-mono">${Math.round(mark.startTime)}ms</span>
+      </div>`
+  }
+
   const load_dm_script = async (id) => {
-    log_mark(performance.mark('dm_script_start'))
+    save_mark('dm_script_start')
     return new Promise((resolve) => {
-      const onLoad = () => {
-        log_mark(performance.mark('dm_script_end'))
+      const onload = () => {
+        save_mark('dm_script_end')
         resolve()
       }
       const script = document.createElement('script')
       script.src = `https://geo.dailymotion.com/libs/player/${id}.js`
       document.querySelector('#dm-script-src').innerHTML = `<code>${script.src}</code>`
-      script.onload = onLoad
+      script.onload = onload
       document.head.appendChild(script)
     })
   }
 
-  /**
-   * Create DM player
-   */
-  const start_dm_player = async () => {
-    log_mark(performance.mark('dm_player_start'))
+  const start_dm_preroll = async () => {
+    save_mark('dm_player_start')
     return new Promise(async (resolve) => {
-      const player = await dailymotion.createPlayer('dm-player', { video: 'x9hnw4q' })
+      const player = await dailymotion.createPlayer('dm-player', {
+        video: 'x9hnw4q',
+        params: {
+          customConfig: {
+            adurl:
+              'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=[CACHEBUSTING]',
+          },
+        },
+      })
       player.on(
-        dailymotion.events.VIDEO_PLAYING,
+        dailymotion.events.AD_IMPRESSION,
         () => {
-          log_mark(performance.mark('dm_player_end'))
+          save_mark('dm_ad_impression')
           setTimeout(player.pause, PLAY_DURATION)
           resolve()
         },
@@ -44,60 +65,77 @@
     })
   }
 
-  /**
-   * Load JW script
-   */
+  const start_dm_adless = async () => {
+    save_mark('dm_player_start')
+    return new Promise(async (resolve) => {
+      const player = await dailymotion.createPlayer('dm-player', { video: 'x9hnw4q' })
+      player.on(
+        dailymotion.events.VIDEO_PLAYING,
+        () => {
+          save_mark('dm_ttff')
+          setTimeout(player.pause, PLAY_DURATION)
+          resolve()
+        },
+        { once: true }
+      )
+    })
+  }
+
   const load_jw_script = async () => {
-    log_mark(performance.mark('jw_script_start'))
+    save_mark('jw_script_start')
     return new Promise((resolve) => {
-      const onLoad = () => {
-        log_mark(performance.mark('jw_script_end'))
+      const onload = () => {
+        save_mark('jw_script_end')
         resolve()
       }
       const script = document.createElement('script')
       script.src = 'https://content.jwplatform.com/libraries/IDzF9Zmk.js'
       document.querySelector('#jw-script-src').innerHTML = `<code>${script.src}</code>`
-      script.onload = onLoad
+      script.onload = onload
       document.head.appendChild(script)
     })
   }
 
-  /**
-   * Create JW player
-   */
-  const start_jw_player = async () => {
-    log_mark(performance.mark('jw_player_start'))
+  const start_jw_preroll = async () => {
+    save_mark('jw_player_start')
+    return new Promise(async (resolve) => {
+      const player = await jwplayer('jw-player').setup({
+        autostart: true,
+        file: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
+        advertising: {
+          client: 'googima',
+          schedule: [
+            {
+              offset: 'pre',
+              tag: 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=',
+              custParams: {},
+            },
+          ],
+        },
+      })
+      player.once('adImpression', () => {
+        save_mark('jw_ad_impression')
+        setTimeout(player.pause, PLAY_DURATION)
+        resolve()
+      })
+    })
+  }
+
+  const start_jw_adless = async () => {
+    save_mark('jw_player_start')
     return new Promise(async (resolve) => {
       const jwPlayer = await jwplayer('jw-player').setup({
         autostart: true,
         file: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
       })
       jwPlayer.once('firstFrame', () => {
-        log_mark(performance.mark('jw_player_end'))
+        save_mark('jw_ttff')
         setTimeout(jwPlayer.pause, PLAY_DURATION)
         resolve()
       })
     })
   }
 
-  /**
-   * Display mark in page
-   * @param {PerformanceMark} mark
-   */
-  const log_mark = (mark) => {
-    marks[mark.name] = mark // Save mark in upper scope for later usage
-    document.querySelector('#logs').innerHTML += `<div class="log-row">
-        <span><code>${mark.name}</code></span>
-        <span class="font-mono">${Math.round(mark.startTime)}ms</span>
-      </div>`
-  }
-
-  /**
-   * Display duration between start and end in page
-   * @param {string} id
-   * @param {PerformanceMark} start
-   * @param {PerformanceMark} end
-   */
   const log_diff = (id, start, end) => {
     const diff = Math.round(end.startTime - start.startTime)
     document.querySelector(id).innerHTML += `<div class="log-row">
@@ -114,22 +152,49 @@
     </div>`
   }
 
+  const run_adless_routine = async (id) => {
+    await load_dm_script(id || 'x136j6')
+    await start_dm_adless()
+    await load_jw_script()
+    await start_jw_adless()
+    log_diff('#dm-diff', marks.dm_script_start, marks.dm_script_end)
+    log_diff('#dm-diff', marks.dm_player_start, marks.dm_ttff)
+    log_total('#dm-diff', marks.dm_script_start, marks.dm_ttff)
+    log_diff('#jw-diff', marks.jw_script_start, marks.jw_script_end)
+    log_diff('#jw-diff', marks.jw_player_start, marks.jw_ttff)
+    log_total('#jw-diff', marks.jw_script_start, marks.jw_ttff)
+  }
+
+  const run_preroll_routine = async (id) => {
+    await load_dm_script(id || 'x15doe')
+    await start_dm_preroll()
+    await load_jw_script()
+    await start_jw_preroll()
+    log_diff('#dm-diff', marks.dm_script_start, marks.dm_script_end)
+    log_diff('#dm-diff', marks.dm_player_start, marks.dm_ad_impression)
+    log_total('#dm-diff', marks.dm_script_start, marks.dm_ad_impression)
+    log_diff('#jw-diff', marks.jw_script_start, marks.jw_script_end)
+    log_diff('#jw-diff', marks.jw_player_start, marks.jw_ad_impression)
+    log_total('#jw-diff', marks.jw_script_start, marks.jw_ad_impression)
+  }
+
   // ┌───────────────────────────────────────────────────────────────────────────────────────────────┐
   // │                                                                                               │
   // │ Runtime                                                                                       │
   // │                                                                                               │
   // └───────────────────────────────────────────────────────────────────────────────────────────────┘
   const params = new URLSearchParams(document.location.search)
-  const id = params.get('id') || 'x136j6'
-  log_mark(performance.mark('origin', { startTime: 0 }))
-  await load_dm_script(id)
-  await start_dm_player()
-  await load_jw_script()
-  await start_jw_player()
-  log_diff('#dm-diff', marks.dm_script_start, marks.dm_script_end)
-  log_diff('#dm-diff', marks.dm_player_start, marks.dm_player_end)
-  log_total('#dm-diff', marks.dm_script_start, marks.dm_player_end)
-  log_diff('#jw-diff', marks.jw_script_start, marks.jw_script_end)
-  log_diff('#jw-diff', marks.jw_player_start, marks.jw_player_end)
-  log_total('#jw-diff', marks.jw_script_start, marks.jw_player_end)
+  const preset = params.get('preset') || 'adless'
+  const id = params.get('id')
+
+  autoreload_on_preset(preset)
+
+  switch (preset) {
+    case 'preroll':
+      run_preroll_routine(id)
+      break
+    case 'adless':
+    default:
+      run_adless_routine(id)
+  }
 })()
